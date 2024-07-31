cf9a41c736f931a7e66ba8059a541a4b29c1012f
Raghuraj Singh <130720974+INFINITY-R13@users.noreply.github.com>
2024-07-31 20:18:07 +0530

    Added Day1 Files

---
+++ variables.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter first no. = "))
+num2 = float(input("enter second no. = "))
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2
+div = num1/num2
+print("sum =", sum)
+print("diff =", diff)
+print("mul =", mul)
+print("div =", div)
